f2ba820941348703ea6dc22790d841b45597c3af
Ryan Charlton <ryancharlton271@gmail.com>
2023-07-14 17:00:28 +0100

    add: glad to load opengl functions

--- Mandlebrot/mandle.cpp
+++ Mandlebrot/mandle.cpp
@@ -1,19 +1,46 @@
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include <iostream>
 
-bool QUIT = false;
+// Square mesh over the screen
+const float screen[] = {
+    -1.0f, -1.0f,
+    -1.0f, 1.0f,
+     1.0f, 1.0f,
+     
+     1.0f, 1.0f,
+     1.0f, -1.0f,
+    -1.0f, -1.0f};
 
 void keycallback(GLFWwindow *win, int key, int scancode, int action, int mods){
     if (key == GLFW_KEY_ESCAPE)
-            QUIT = true;
+            glfwSetWindowShouldClose(win, GLFW_TRUE);
 }
 
 int main(){
-    glfwInit();
+    if(!glfwInit()){
+        std::cout << "GLFW failed to initialize" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
     GLFWwindow *window = glfwCreateWindow(800, 600, "Mandlebrot", NULL, NULL);
+    if(!window){
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    glfwSwapInterval(1);
+
     glfwSetKeyCallback(window, keycallback);
 
-    while(!QUIT){
+    
+
+    while(!glfwWindowShouldClose(window)){
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
